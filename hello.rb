b3aa45bced738440ead4c0095319fcff25462745
Hiroya Kabata <h.kabata420@gmail.com>
2014-02-22 10:14:22 +0900

    interaction

---
+++ hello.rb
@@ -0,0 +1,6 @@
+print 'What\'s your last name? : ' 
+last_name = gets.chomp
+print 'What\'s your first name? : ' 
+first_name = gets.chomp
+puts 'Hello,' + first_name + ' ' + last_name + ' !'
+
---
+++ suggest_number.rb
@@ -0,0 +1,3 @@
+print 'What\'s your favorite number? : '
+num = gets.chomp
+puts 'Your lucky number is ' + (num.to_i() + 1).to_s() + '!'
