0841e9b6e44e0ae0d003fd84109656ebb6d1a0d1
Hiroya Kabata <h.kabata420@gmail.com>
2014-02-22 09:41:34 +0900

    number

---
+++ calc.rb
@@ -0,0 +1,19 @@
+print "A year is " 
+print 24 * 365
+print " hours.\n"
+
+print "10 years are "
+print 60 * 24 * 365 * 10
+print " minutes.\n"
+
+print (Time.now() - Time.mktime(1988, 4, 20)).to_i()
+print " seconds has passed from I was born.\n"
+
+print "I wish I could eat "
+print 2 * 365 * 30
+print " chocolates in my whole life. \n"
+
+print "You are "
+print 1034000000 / (60 * 60 * 24 * 365)
+print " years old, "
+print "if 1,034,000,000 seconds have passed from your birth.\n"
